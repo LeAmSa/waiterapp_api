36a3ee9b70474bc24a0ef8f57b08b0d09ff6f4de
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-11-28 21:35:41 -0300

    Add socket.io config

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,34 @@
 import path from "node:path";
-
+import http from "node:http";
 import express from "express";
-
 import mongoose from "mongoose";
-
 import { router } from "./router";
+import { Server } from "socket.io";
 
 import * as dotenv from "dotenv";
 dotenv.config();
 
 const databaseUrl = process.env.DATABASE_URL;
 
+const app = express();
+
+const server = http.createServer(app);
+export const io = new Server(server);
+
 mongoose
   .connect(databaseUrl as string)
   .then(() => {
-    const app = express();
+    io.on("connect", () => {
+      console.log("Conectou!");
+    });
+
+    app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "*");
+      res.setHeader("Access-Control-Allow-Headers", "*");
+
+      next();
+    });
 
     app.use(
       "/uploads",
@@ -23,7 +37,7 @@ mongoose
     app.use(express.json());
     app.use(router);
 
-    app.listen(3333, () => {
+    server.listen(3333, () => {
       console.log("Server is running on http://localhost:3333");
     });
 
