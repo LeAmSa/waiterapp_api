e37c807064469a3fe1d351ed62f16348da165ec7
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-11-28 21:36:29 -0300

    Add socket emit on create order request

--- api/app/useCases/orders/createOrder.ts
+++ api/app/useCases/orders/createOrder.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 
 import { Order } from "../../models/Order";
 
+import { io } from "../../..";
+
 export async function createOrder(req: Request, res: Response) {
   try {
     const { table, products } = req.body;
@@ -10,6 +12,10 @@ export async function createOrder(req: Request, res: Response) {
       table,
       products,
     });
+
+    const orderDetails = await order.populate("products.product");
+
+    io.emit("orders@new", orderDetails);
 
     res.status(201).json(order);
   } catch (error) {
